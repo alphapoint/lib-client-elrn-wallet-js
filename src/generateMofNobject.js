868948c8e7aca69ae7e78bd67a4ae265215360ec
scmilee <drudged@oregonstate.edu>
2018-07-26 14:26:57 -0700

    add a function that takes in any number of regular public keys and an integer and returns an MofN object that holds the address, the redeem outputs etc.

---
+++ src/generateMofNobject.js
@@ -0,0 +1,17 @@
+const bitcoin = require('bitcoinjs-lib');
+const bufferFrom = require('buffer-from');
+
+export default function generateMofNaddress(pubKeys, requiredSignatures ) {
+  if (requiredSignatures > pubKeys.length ) {return Promise.reject(new Error('You can\'t require more signatures than are in the provided pubKey array'));}
+  if (!Array.isArray(pubKeys) ) {return Promise.reject(new Error('You must call generateMofNaddress with a pubkey array'));}
+  
+      try {
+          const buffedPubkeys = pubKeys.map((hex) => Buffer.from(hex, 'hex'))
+          const multisig = bitcoin.payments.p2sh({
+            redeem: bitcoin.payments.p2ms({m: requiredSignatures, pubkeys:buffedPubkeys})
+          })
+          return multisig;
+      } catch (err) {
+          return Promise.reject(err);
+        }
+}
